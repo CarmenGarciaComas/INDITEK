7b3e983248fb0c4a29ac5f778fa25da03dc6a57b
CarmenGarciaComas <90462832+CarmenGarciaComas@users.noreply.github.com>
2022-03-02 12:22:29 +0100

    Update inditek_figures.m

--- inditek_figures.m
+++ inditek_figures.m
@@ -5,64 +5,8 @@ addpath(genpath('~/toolbox/'))
 %
 disp('CHECK (1) that in you have downloaded m_maps to your toolbox folder, and (2) that you are in the main folder with files: INDITEKexponential_grid.mat & INDITEKlogistic_grid.mat for figure 1 / INDITEKexponential_global.mat & INDITEKlogistic_global.mat for figure 2 / inditek_LogisticModel_KminKmax.mat for figure 3. If it is OK press any key to continue')
 pause
-%################################ FIGURE 1: DIVERSITY MAPS ################################
-m_proj('Mollweide','clongitude',0);
-timeframes=[25,36,69,82];
-load INDITEKexponential_grid.mat
-TF=[400,300,70,0];
-Dexp=D(:,:,timeframes);
-M=log10(256);
-load INDITEKlogistic_grid.mat
-Dlog=D(:,:,timeframes);
-close all
-figure
-set(gcf, 'PaperUnits', 'centimeters','papersize',[15 20])
-set(gcf,'paperposition',[0 0 15 20])
-POS=[0.01 0.74 0.45 0.18;0.52 0.74 0.45 0.18;0.01 0.53 0.45 0.18;0.52 0.53 0.45 0.18;0.01 0.32 0.45 0.18;0.52 0.32 0.45 0.18;0.008 0.11 0.45 0.18;0.52 0.11 0.45 0.18];
-Let={'a','e','b','f','c','g','d','h'};
-sp=0;
-for i=1:length(timeframes)
-    sp=sp+1;
-    subplot(4,2,sp)
-    set(gca,'position',POS(sp,:))
-    m_pcolor(X,Y,log10(Dlog(:,:,i)));
-    m_grid('xaxis','middle','xticklabel',[],'yticklabel',[],'backcolor',[0.9 0.9 0.9],'LineWidth',0.6,'linestyle',':');
-    a=title([num2str(TF(i)),' MA']);
-    set(a,'FontName','arial','fontsize',12)
-    a=text(-2,1,Let{sp});
-    set(a,'FontName','arial','fontsize',12)
-    caxis([log10(1),M])
-    colormap(jet(50));
-    if sp==1
-        a=text(-0.96,1.8,'Logistic model');
-        set(a,'FontName','arial','fontsize',12,'fontweight','bold')
-    end
-    sp=sp+1;
-    subplot(4,2,sp)
-    set(gca,'position',POS(sp,:))
-    m_pcolor(X,Y,log10(Dexp(:,:,i)));
-    m_grid('xaxis','middle','xticklabel',[],'yticklabel',[],'backcolor',[0.9 0.9 0.9],'LineWidth',0.1,'linestyle',':');
-    a=title([num2str(TF(i)),' MA']);
-    set(a,'FontName','arial','fontsize',12)
-    a=text(-2,1,Let{sp});
-    set(a,'FontName','arial','fontsize',12)
-    caxis([log10(1),M])
-    colormap(jet(50));
-    if sp==2
-        a=text(-1.18,1.8,'Exponential model');
-        set(a,'FontName','arial','fontsize',12,'fontweight','bold')
-    end
-    if i==length(timeframes)
-        h = colorbar('location','SouthOutside','Position',[0.55,5,0.35,0.03]);
-        set(h,'xtick',[log10(1),log10(2),log10(4),log10(8),log10(16),log10(32),log10(64),log10(128),log10(256)],'xticklabel',{'1','2','4','8','16','32','64','128','256'})
-        set(h,'position',[0.32,0.08,0.3,0.016],'FontName','arial','fontsize',9)
-        text(-4,-2.1,'Diversity (# genera area^-^1)','FontName','arial','fontsize',12);
-    end
-    
-end
-print ('Inditek_Figure1','-djpeg', '-r200')
-close
-% ################################### FIGURE 2: TIME SERIES ##################
+
+% ################################### FIGURE 1: TIME SERIES ##################
 
 clear all;close all;clc
 figure
@@ -72,6 +16,8 @@ load INDITEKexponential_global
 Dexp=gammaD;
 load INDITEKlogistic_global
 Dlog=gammaD;
+load INDITEKcaliblogistic_global
+Dcalib=gammaD;
 [d,T]=xlsread('data/FossilTimeSeries');
 if ext_pattern==3
     ini=4;
@@ -90,12 +36,13 @@ elseif ext_pattern==3
 end
 d1=d1(isnan(d1)==0);t=t(isnan(d1)==0);
 d1=interp1(t,d1,-1*Point_timeslices,'linear');
-[AX H1 H2]=plotyy(-1*Point_timeslices, [Dexp,Dlog], -1*Point_timeslices, d1, 'plot');
+[AX H1 H2]=plotyy(-1*Point_timeslices, [Dexp,Dlog,Dcalib], -1*Point_timeslices, d1, 'plot');
 set(AX(1),'linewidth',0.75,'FontName','arial','fontsize',12);
 set(AX(2),'linewidth',0.75,'FontName','arial','fontsize',12);
 set(H1(1),'LineStyle','-','Color','b','LineWidth',1,'Marker','o','MarkerSize',3,'MarkerEdgeColor','b','Markerfacecolor','b')
 set(H1(2),'LineStyle','-','Color','r','LineWidth',1,'Marker','o','MarkerSize',3,'MarkerEdgeColor','r','Markerfacecolor','r')
 set(H2(1),'LineStyle',':','Color',[0.45,0.45,0.45],'LineWidth',1.1)
+set(H1(3),'LineStyle','--','Color','b','LineWidth',0.7)
 set(AX,{'ycolor'},{'k';'k'})
 set(AX,'xlim',[-541,0],'xtick',[])
 set(AX(1),'xlim',[-541,0],'xtick',[-500:100:0])
@@ -115,10 +62,11 @@ xh = get(AX(2),'Ylabel');
 p = get(xh,'position');
 p(1) = 1.15*p(1);
 set(xh,'position',p);
+set(AX,{'ycolor'},{'k';'k'})
 a=xlabel('Time (MA)');
 set(a,'FontName','arial','fontsize',12);
 hold on
-a=legend('Exponential diversification','Logistic diversification','Fossil record');
+a=legend('Exponential','Saturated logistic','Calibrated logistic','Fossil record');
 legend boxoff
 set(a,'Location','Northwest','FontName','arial','fontsize',12);
 if ext_pattern==1
@@ -201,30 +149,80 @@ elseif ext_pattern==2
 elseif ext_pattern==3
     NaNpos=[15:17,26:28,31:33,41:44,49:50,70];
 end
+print ('Inditek_Figure1','-djpeg', '-r200')
+
+%################################ FIGURE 2: DIVERSITY MAPS ################################
+m_proj('Mollweide','clongitude',0);
+timeframes=[7,16,25,36,69,82];
+TF=[500,450,400,300,70,0];
+load INDITEKcaliblogistic_grid
+D=D(:,:,timeframes);
+M=log10(128);
+close all
+figure
+set(gcf, 'PaperUnits', 'centimeters','papersize',[20 10])
+set(gcf,'paperposition',[0 0 20 10])
+POS=[0.01 0.55 0.3 0.47;0.34 0.55 0.3 0.47; 0.67 0.55 0.3 0.47; 0.01 0.1 0.3 0.47;0.34 0.1 0.3 0.47; 0.67 0.1 0.3 0.47];
+%Let={'a','e','b','f','c','g','d','h'};
+Let={'a','b','c','d','e','f'};
+
+sp=0;
+for i=1:length(timeframes)
+    sp=sp+1;
+    subplot(2,3,sp)
+    set(gca,'position',POS(sp,:))
+    m_pcolor(X,Y,log10(D(:,:,i)));
+    m_grid('xaxis','middle','xticklabel',[],'yticklabel',[],'backcolor',[0.9 0.9 0.9],'LineWidth',0.6,'linestyle',':');
+    a=title([num2str(TF(i)),' MA']);
+    set(a,'FontName','arial','fontsize',12)
+    a=text(-2,1,Let{sp});
+    set(a,'FontName','arial','fontsize',12)
+    caxis([log10(1),M])
+    colormap(jet(50));
+   
+    if i==length(timeframes)
+        h = colorbar('location','SouthOutside','Position',[0.05,5,0.35,0.03]);
+        set(h,'xtick',[log10(1),log10(2),log10(4),log10(8),log10(16),log10(32),log10(64),log10(128),log10(256)],'xticklabel',{'1','2','4','8','16','32','64','128','256'})
+        set(h,'position',[0.35,0.1,0.3,0.03],'FontName','arial','fontsize',9)
+        text(-6,-2.1,'Diversity (# genera area^-^1)','FontName','arial','fontsize',12);
+    end
+    
+end
 print ('Inditek_Figure2','-djpeg', '-r200')
 
+close
 
-%######################### FIGURE 3: KminKmax CCC ##################################
-
-load 'inditek_LogisticModel_KminKmax'
+%######################### FIGURE 3: DIVERSITY/CARRYING CAPACITY MAPS ##################################
+load INDITEKcaliblogistic_AlphabyK
+D=D(:,:,timeframes);
 figure
-set(gcf, 'PaperUnits', 'centimeters','papersize',[10 8])
-set(gcf,'paperposition',[0 0 9 7])
-CCC(CCC==0)=NaN;
-imAlpha=ones(size(CCC));
-imAlpha(isnan(CCC))=0;
-imagesc(CCC,'AlphaData',imAlpha);
-imagesc(1:size(CCC,1),1:size(CCC,2),CCC,'AlphaData',imAlpha)
-colormap(jet(50));
-set(gca,'Fontname','arial','fontsize',9)
-a=xlabel('Kmin (# genera area^-^1)');
-set(a,'Fontname','arial','fontsize',9);
-a=ylabel('Kmax (# genera area^-^1)');
-set(a,'Fontname','arial','fontsize',9);
-set(gca, 'ytick',[1:size(CCC,1)],'yticklabel',{'256','128','64','32','16','8','4','2'},'xtick',[1:size(CCC,1)],'xticklabel',{'2','4','8','16','32','64','128','256'},'linewidth',1.2);
-caxis([0,1])
-colorbar
-hold on
+set(gcf, 'PaperUnits', 'centimeters','papersize',[20 10])
+set(gcf,'paperposition',[0 0 20 10])
+
+sp=0;
+for i=1:length(timeframes)
+    sp=sp+1;
+    subplot(2,3,sp)
+    set(gca,'position',POS(sp,:))
+    m_pcolor(X,Y,log10(D(:,:,i)+10^-16);
+    m_grid('xaxis','middle','xticklabel',[],'yticklabel',[],'backcolor',[0.9 0.9 0.9],'LineWidth',0.6,'linestyle',':');
+    a=title([num2str(TF(i)),' MA']);
+    set(a,'FontName','arial','fontsize',12)
+    a=text(-2,1,Let{sp});
+    set(a,'FontName','arial','fontsize',12)
+    caxis([log10(1),M])
+    colormap(jet(50));
+   
+    if i==length(timeframes)
+        h = colorbar('location','SouthOutside','Position',[0.05,5,0.35,0.03]);
+        set(h,'xtick',[log10(0.1),log10(0.15),log10(0.25),log10(0.4),log10(0.65),log10(1)],'xticklabel',{'0.1','0.15','0.25','0.4','0.65','1'})
+        set(h,'position',[0.35,0.1,0.3,0.03],'FontName','arial','fontsize',9)
+        text(-6,-2.1,'Diversity/K_e_f_f','FontName','arial','fontsize',12);
+    end
+    
+end
+print ('Inditek_Figure3','-djpeg', '-r200')
+
 print ('Inditek_Figure3','-djpeg', '-r200')
 
 clear all;close all;clc
